ad06ce0336cb928323abc8bfbf0ee91396b43cb0
zivzolti-lab <zivzolti@gmail.com>
2025-11-12 19:42:29 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,10 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    leibniz_series = []
+    for i in range(n_terms):
+        if (i % 2) == 0:
+            leibniz_series.append(1 / ((i * 2) + 1))
+        else: 
+            leibniz_series.append(-1 / ((i * 2) + 1))
+    leibniz_series_sum = sum(leibniz_series)
+    approximate_pi = leibniz_series_sum * 4 
+    return approximate_pi
